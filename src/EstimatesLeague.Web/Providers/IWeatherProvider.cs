82f1a804f4c98de9ffd31872dd563355893d0741
Önder Genç <ondergenc@Onder-MacBook-Pro.local>
2019-02-07 23:09:02 +0300

    Create project and solution

---
+++ src/EstimatesLeague.Web/Providers/IWeatherProvider.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using EstimatesLeague.Web.Models;
+
+namespace EstimatesLeague.Web.Providers
+{
+    public interface IWeatherProvider
+    {
+        List<WeatherForecast> GetForecasts();
+    }
+}
